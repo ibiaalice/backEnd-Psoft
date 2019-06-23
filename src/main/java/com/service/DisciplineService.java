36d72af516e124eaff530a5fb56ca0889212c9fc
Bia Alice <beatrizalice00@gmail.com>
2019-06-23 01:05:33 -0300

    essa noite não tem luar

--- src/main/java/com/service/DisciplineService.java
+++ src/main/java/com/service/DisciplineService.java
@@ -1,18 +1,32 @@
 package com.service;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.tomcat.util.json.ParseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.dao.DisciplineDAO;
 import com.model.Discipline;
+
+import net.minidev.json.parser.JSONParser;
+
+
+
 
 @Service
 public class DisciplineService {
 	
 	private final DisciplineDAO disciplineDAO;
 	
-	public DisciplineService(DisciplineDAO disciplineDAO) {
+	public DisciplineService(DisciplineDAO disciplineDAO) throws ParseException, IOException, Exception {
 		this.disciplineDAO = disciplineDAO;
+		//this.saveAllDisciplines();
+		
 	}
 	
 	public Discipline create(Discipline discipline) {
@@ -68,6 +82,40 @@ public class DisciplineService {
 	
 	public List<Discipline> findAll(){
 		return this.disciplineDAO.findAll();
+	}
+	
+	public void createAllDiscipline(Discipline discipline) {
+		 disciplineDAO.save(discipline);
+	}
+	
+public void saveAllDisciplines()  throws IOException, ParseException, Exception{
+	JSONArray jsonArray;
+	
+	JSONParser jsonParser = new JSONParser();
+	FileReader disciplines = new FileReader("src/main/java/com/util/disciplinas.json");
+	
+	jsonArray = (JSONArray) jsonParser.parse(disciplines);
+	
+	//aqui que a mágica acontece:
+	((Collection) jsonArray).stream().forEach(discipline ->{
+		JSONObject object = (JSONObject) discipline;
+		
+		
+		String name = "";
+		//a chance de isso tá errada é de mais de 3000 pontos de poder
+		try {
+			name = (String) object.get("nome");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+				
+		this.create(new Discipline(name));
+		
+		
+	});
+		
 	}
 	
 }
