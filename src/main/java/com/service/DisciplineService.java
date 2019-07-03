0e7c96fbc35bada05fb72e16d970cde41958200b
Beatriz Alice <beatriz.azevedo@ccc.ufcg.edu.br>
2019-07-02 22:07:35 -0300

    adiciona umas paradas do bd

--- src/main/java/com/service/DisciplineService.java
+++ src/main/java/com/service/DisciplineService.java
@@ -2,17 +2,14 @@ package com.service;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 
-import com.dao.UserDAO;
-import com.model.Usuario;
+import com.dao.CommentDAO;
+import com.model.Comment;
 import exception.DisciplineNotFoundException;
 import org.apache.tomcat.util.json.ParseException;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dao.DisciplineDAO;
@@ -22,17 +19,19 @@ import com.model.Discipline;
 public class DisciplineService {
 
 	private final DisciplineDAO disciplineDAO;
-
+	private final CommentDAO commentDAO;
 
 	/**
 	 * Método de construção da classe
 	 * @param disciplineDAO
+	 * @param commentDAO
 	 */
-	public DisciplineService(DisciplineDAO disciplineDAO)  {
+	public DisciplineService(DisciplineDAO disciplineDAO, CommentDAO commentDAO)  {
 
 		this.disciplineDAO = disciplineDAO;
+		this.commentDAO = commentDAO;
 		//this.deleteAll();
-		/*try {
+		try {
 			this.saveAll();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -40,7 +39,7 @@ public class DisciplineService {
 			e.printStackTrace();
 		} catch (JSONException e) {
 			e.printStackTrace();
-		}*/
+		}
 
 	}
 
@@ -178,10 +177,13 @@ public class DisciplineService {
 		JSONArray array = new JSONArray("[{\"nome\":\"ADMINISTRAÇÃO\"},{\"nome\":\"ADMINISTRAÇÃO DE SISTEMAS\"},{\"nome\":\"ADMINISTRAÇÃO DE SISTEMAS DE INFORMAÇÃO\"},{\"nome\":\"ALGEBRA LINEAR I\"},{\"nome\":\"ALGORITMOS AVANÇADOS I\"},{\"nome\":\"ALGORITMOS AVANCADOS II\"},{\"nome\":\"ALGORITMOS AVANCADOS III\"},{\"nome\":\"ANÁLISE DE SISTEMAS\"},{\"nome\":\"ANÁLISE E TÉCNICAS DE ALGORITMOS\"},{\"nome\":\"APLICAÇÕES DE GRAFOS\"},{\"nome\":\"APLICAÇÕES DE PLP\"},{\"nome\":\"AVALIAÇÃO DE DESEMPENHO DE SISTEMAS DISCRETOS\"},{\"nome\":\"BANCO DE DADOS 1\"},{\"nome\":\"BANCO DE DADOS 2\"},{\"nome\":\"BASQUETEBOL FEM\"},{\"nome\":\"BASQUETEBOL MAS\"},{\"nome\":\"CALCULO DIFERENCIAL E INTEGRAL I\"},{\"nome\":\"CALCULO DIFERENCIAL E INTEGRAL II\"},{\"nome\":\"CÁLCULO NUMÉRICO\"},{\"nome\":\"CIÊNCIA DE DADOS DESCRITIVA\"},{\"nome\":\"CIÊNCIA DE DADOS PREDITIVA\"},{\"nome\":\"COMPILADORES\"},{\"nome\":\"DESENVOLVIMENTO DE APLICAÇÕES CORPORATIVAS AVANÇADAS\"},{\"nome\":\"DIREITO E CIDADANIA\"},{\"nome\":\"ECONOMIA\"},{\"nome\":\"ECONOMIA DE TECNOLOGIA DA INFORMAÇÃO\"},{\"nome\":\"EMPREENDEDORISMO\"},{\"nome\":\"EMPREENDEDORISMO EM SOFTWARE\"},{\"nome\":\"ENGENHARIA DE SOFTWARE\"},{\"nome\":\"ESTÁGIO INTEGRADO\"},{\"nome\":\"ESTÁGIO INTEGRADO I\"},{\"nome\":\"ESTAGIO INTEGRADO II\"},{\"nome\":\"ESTAGIO INTEGRADO III\"},{\"nome\":\"ESTATÍSTICA APLICADA\"},{\"nome\":\"ESTRUTURA DE DADOS\"},{\"nome\":\"FUNDAMENTOS DE MATEMATICA PARA CIÊNCIA DA COMPUTACAO I\"},{\"nome\":\"FUNDAMENTOS DE MATEMATICA PARA CIÊNCIA DA COMPUTACAO II\"},{\"nome\":\"FUNDAMENTOS DE PROGRAMAÇÃO CONCORRENTE\"},{\"nome\":\"FUTSAL\"},{\"nome\":\"GERÊNCIA DE REDES DE COMPUTADORES\"},{\"nome\":\"GESTÃO DE PROJETOS\"},{\"nome\":\"GESTÃO DE SISTEMAS DE INFORMAÇÃO\"},{\"nome\":\"GOVERNÂNCIA DA INTERNET\"},{\"nome\":\"INFORMÁTICA E SOCIEDADE\"},{\"nome\":\"INGLÊS\"},{\"nome\":\"INTELIGÊNCIA ARTIFICIAL\"},{\"nome\":\"INTERCONEXÃO DE REDES DE COMPUTADORES\"},{\"nome\":\"INTRODUÇÃO A BANCO DE DADOS E MINERAÇÃO DE DADOS\"},{\"nome\":\"INTRODUÇÃO À CIÊNCIA DA COMPUTAÇÃO\"},{\"nome\":\"INTRODUÇÃO À COMPUTAÇÃO\"},{\"nome\":\"INTRODUÇÃO À PROBABILIDADE\"},{\"nome\":\"JOGOS DIGITAIS\"},{\"nome\":\"LABORATÓRIO DE ENGENHARIA DE SOFTWARE\"},{\"nome\":\"LABORATÓRIO DE ESTRUTURA DE DADOS\"},{\"nome\":\"LABORATÓRIO DE INTERCONEXÃO DE REDES DE COMPUTADORES\"},{\"nome\":\"LABORATÓRIO DE ORGANIZAÇÃO E ARQUITETURA DE COMPUTADORES\"},{\"nome\":\"LABORATÓRIO DE PROGRAMAÇÃO 1\"},{\"nome\":\"LABORATÓRIO DE PROGRAMAÇÃO 2\"},{\"nome\":\"LEITURA E PRODUÇÃO DE TEXTO\"},{\"nome\":\"LÍNGUA PORTUGUESA\"},{\"nome\":\"LÓGICA PARA COMPUTAÇÃO\"},{\"nome\":\"METODOLOGIA CIENTÍFICA\"},{\"nome\":\"MÉTODOS E SOFTWARE NUMÉRICOS\"},{\"nome\":\"MÉTODOS ESTATÍSTICOS\"},{\"nome\":\"ORGANIZAÇÃO E ARQUITETURA DE COMPUTADORES\"},{\"nome\":\"PARADIGMAS DE LINGUAGENS DE PROGRAMAÇÃO\"},{\"nome\":\"PERCEPÇÃO COMPUTACIONAL\"},{\"nome\":\"PRÁTICA DE ENSINO DE COMPUTAÇÃO I\"},{\"nome\":\"PRÁTICA DE ENSINO DE COMPUTAÇÃO II\"},{\"nome\":\"PRINCÍPIOS DE DESENVOLVIMENTO WEB\"},{\"nome\":\"PROGRAMAÇÃO 1\"},{\"nome\":\"PROGRAMAÇÃO 2\"},{\"nome\":\"PROGRAMAÇÃO CONCORRENTE\"},{\"nome\":\"PROGRAMAÇÃO FUNCIONAL\"},{\"nome\":\"PROJETO DE REDES DE COMPUTADORES\"},{\"nome\":\"PROJETO DE SISTEMAS OPERACIONAIS\"},{\"nome\":\"PROJETO DE SOFTWARE\"},{\"nome\":\"PROJETO DE TRABALHO DE CONCLUSAO DE CURSO\"},{\"nome\":\"PROJETO EM COMPUTAÇÃO 1\"},{\"nome\":\"PROJETO EM COMPUTAÇÃO 2\"},{\"nome\":\"RECUPERAÇÃO DA INFORMAÇÃO E BUSCA NA WEB\"},{\"nome\":\"REDES DE COMPUTADORES\"},{\"nome\":\"SEMINÁRIOS\"},{\"nome\":\"SEMINÁRIOS (EDUCAÇÃO AMBIENTAL)\"},{\"nome\":\"SISTEMAS DE APOIO À DECISÃO\"},{\"nome\":\"SISTEMAS DE INFORMAÇÃO II\"},{\"nome\":\"SISTEMAS DE RECUPUPERAÇÃO DA INFORMAÇÃO\"},{\"nome\":\"SISTEMAS OPERACIONAIS\"},{\"nome\":\"SOCIOLOGIA INDUSTRIAL I\"},{\"nome\":\"TÉCNICAS DE PROGRAMAÇÃO\"},{\"nome\":\"TEORIA DA COMPUTAÇÃO\"},{\"nome\":\"TEORIA DOS GRAFOS\"},{\"nome\":\"TRABALHO DE CONCLUSAO DE CURSO\"},{\"nome\":\"VERIFICACAO E VALIDAÇÃO DE SOFTWARE\"},{\"nome\":\"VISÃO COMPUTACIONAL\"},{\"nome\":\"VISUALIZAÇÃO DE DADOS\"}]");
 		for(int i = 0 ; i < array.length() ; i++){
 			list.add((String) array.getJSONObject(i).get("nome"));
+
 		}
 
-		for(int i = 0; i < list.size(); i++)
+		for(int i = 0; i < list.size(); i++){
 			this.create(new Discipline(list.get(i)));
+			this.commentDAO.save(new Comment()); //isso é só pra add a quantidade de elementos no bd
+		}
 	}
 
 }
